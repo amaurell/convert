8a963d1bed2db246bc8ec4f69ac8814a8ec05aac
Andre Maurell <amaurell@gmail.com>
2025-02-27 13:45:51 -0300

    Pega a moeda selecionada pelo usuário

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
+const form = document.querySelector("form");
 
 //Manipulando o input  amount para receber somente números
 amount.addEventListener("input", () => {
@@ -7,3 +9,9 @@ amount.addEventListener("input", () => {
   //Procura letras no input e substitui por ""(nada)
   amount.value = amount.value.replace(hasCharacterRegex, "");
 });
+
+//Capiturando o envento de submit do formulário
+form.onsubmit = (event) => {
+  event.preventDefault();
+  console.log(currency.value);
+};
