ed271bc5f42f2dd73d23513981f718bc2b490fd5
Andre Maurell <amaurell@gmail.com>
2025-02-27 13:39:20 -0300

    Criou um regex para não aceitar letras no campo de input

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("carregado");
+const amount = document.getElementById("amount");
+
+//Manipulando o input  amount para receber somente números
+amount.addEventListener("input", () => {
+  const hasCharacterRegex = /\D+/g;
+
+  //Procura letras no input e substitui por ""(nada)
+  amount.value = amount.value.replace(hasCharacterRegex, "");
+});
